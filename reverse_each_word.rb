023c3125947630946843e56661b663549e413be6
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-08-30 06:33:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   wordArray = string.split(" ")
-  wordArray.each(){|el| el.reverse}
-  wordArray.join(" ")
+  newArray = wordArray.map(){|el| el.reverse}
+  newArray.join(" ")
 end 
